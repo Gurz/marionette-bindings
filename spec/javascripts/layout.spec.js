014fac87b1c2c5b6321a7ecaf550a695ddfbd275
Jarrod Overson <joverson@onerecovery.com>
2012-07-27 13:58:45 -0700

    adding tests for inline regions

--- spec/javascripts/layout.spec.js
+++ spec/javascripts/layout.spec.js
@@ -223,4 +223,39 @@ describe("layout", function(){
     });
   });
 
+  describe("when a template has inlined region definitions", function(){
+    var layout, view1, view2;
+
+    beforeEach(function(){
+      loadFixtures("inlineRegionDefinition.html");
+
+      layout = new Backbone.Marionette.Layout({
+        template : '#inlineRegionDefinition'
+      });
+      layout.render();
+      view1 = new Backbone.Marionette.View();
+      view2 = new Backbone.Marionette.View();
+    });
+
+    it("should instantiate the specified region managers", function(){
+      layout.regionOne.show(view1);
+      layout.regionTwo.show(view2);
+      expect(layout).toHaveOwnProperty("regionOne");
+      expect(layout).toHaveOwnProperty("regionTwo");
+      expect(layout.regionOne.$el.children().length).toBeGreaterThan(0);
+      expect(layout.regionTwo.$el.children().length).toBeGreaterThan(0);
+    });
+
+    it("should close views after rerender", function(){
+      layout.regionOne.show(view1);
+      spyOn(view1,'close');
+      layout.render();
+      expect(view1.close).toHaveBeenCalled();
+      expect(layout.regionOne.currentView).toBeUndefined();
+      layout.regionOne.show(view2);
+      expect(layout.regionOne.currentView).toEqual(view2);
+    });
+
+  });
+
 });
